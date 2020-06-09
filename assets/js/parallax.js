12f49e9458af2d785ef34945e2bba628cac9ed6a
Vijayan <vijayan.m@harman.com>
2020-06-09 17:32:23 +0530

    Reverted parallax js file

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -32,17 +32,17 @@ let tl2 = new TimelineMax()
 				.from($(".parallax1extra, .parallax2, .parallax3, .parallax4, .parallax5, .parallax6"), 1, {x: "0%", y: "100%", autoAlpha:0})
 				/* .to(".parallax1", 1, {x: "0%", y: "0%", autoAlpha:1})
         .to(".parallax1", 1, {x: "0%", y: "-=10%", autoAlpha:0}) */
-        .to(".parallax1extra", -2, {x: "-200%", y: "-300%", autoAlpha:0})
-        .to(".parallax1extra", 2, {x: "-200%", y: "-300%", autoAlpha:1})
-				.to(".parallax2", 5, {x: "0%", y: "0%", autoAlpha:1})
-				.to(".parallax2", 5, {x: "0%", y: "-=0%", autoAlpha:0})
-        .to(".parallax3", 5, {x: "0%", y: "0%", autoAlpha:1})
-        .to(".parallax3", 5, {x: "0%", y: "-=0%", autoAlpha:0})
-        .to(".parallax4", 5, {x: "0%", y: "0%", autoAlpha:1})
-        .to(".parallax4", 5, {x: "0%", y: "-=0%", autoAlpha:0})
-        .to(".parallax5", 5, {x: "0%", y: "0%", autoAlpha:1})
-        .to(".parallax5", 5, {x: "0%", y: "-=0%", autoAlpha:0})
-        .to(".parallax6", 5, {x: "0%", y: "0%", autoAlpha:1});
+        .to(".parallax1extra", 1, {x: "-200%", y: "-300%", autoAlpha:0})
+        .to(".parallax1extra", 1, {x: "-200%", y: "-300%", autoAlpha:1})
+				.to(".parallax2", 1, {x: "0%", y: "0%", autoAlpha:1})
+				.to(".parallax2", 1, {x: "0%", y: "-=0%", autoAlpha:0})
+        .to(".parallax3", 1, {x: "0%", y: "0%", autoAlpha:1})
+        .to(".parallax3", 1, {x: "0%", y: "-=0%", autoAlpha:0})
+        .to(".parallax4", 1, {x: "0%", y: "0%", autoAlpha:1})
+        .to(".parallax4", 1, {x: "0%", y: "-=0%", autoAlpha:0})
+        .to(".parallax5", 1, {x: "0%", y: "0%", autoAlpha:1})
+        .to(".parallax5", 1, {x: "0%", y: "-=0%", autoAlpha:0})
+        .to(".parallax6", 1, {x: "0%", y: "0%", autoAlpha:1});
 
 
 tl2.duration(tl1.duration());
@@ -54,5 +54,5 @@ slideAnimation.add(tl1, 0).add(tl2, 0);
 new ScrollMagic.Scene({
   triggerElement: intro,
   triggerHook: "onLeave",
-  duration: 9000
+  duration: 3000
 }).setPin(intro).setTween(slideAnimation).addTo(controller);
